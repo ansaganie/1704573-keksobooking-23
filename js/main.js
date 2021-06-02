46d03cd8d7cbdafcc8f3d258ac31e0f229712bc3
ansagan <islamgaliev@mail.ru>
2021-06-02 09:24:28 +0500

    'getRandomDecimal' function was relized according to task requirements

--- js/main.js
+++ js/main.js
@@ -7,8 +7,22 @@ let getRandomInt = (from, to) => {
   };
 
   let randomInt = Math.random(); //0-0.99;
-  //console.log('random int' + randomInt);
   let randomNegNumber = randomInt * (from - to); //getting random negative integer
   let wholeNumber = Math.round(randomNegNumber);
   return wholeNumber + to; //makes random number positive
+}
+
+
+let getRandomDecimal = (from, to, precision) => {
+  if (from == to) {
+    return from;
+  }
+  if (from < 0 || from > to) {
+    return undefined
+  };
+
+  let randomInt = Math.random();
+  let randomNegNumber = randomInt * (from - to);
+  let result = randomNegNumber + to;
+  return result.toFixed(precision);
 }
