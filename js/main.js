466e1b8c68bb0d83e4592ad47637a5288ee094d1
ansagan <islamgaliev@mail.ru>
2021-06-02 09:34:57 +0500

    code clean up

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
-let getRandomInt = (from, to) => {
-  if (from == to) {
+const getRandomInt = (from, to) => {
+  if (from === to) {
     return from;
   }
   if (from < 0 || from > to) {
     return undefined
   };
 
-  let randomInt = Math.random(); //0-0.99;
-  let randomNegNumber = randomInt * (from - to); //getting random negative integer
-  let wholeNumber = Math.round(randomNegNumber);
+  const randomInt = Math.random(); //0-0.99;
+  const randomNegNumber = randomInt * (from - to); //getting random negative integer
+  const wholeNumber = Math.round(randomNegNumber);
   return wholeNumber + to; //makes random number positive
-}
+};
 
 
-let getRandomDecimal = (from, to, precision) => {
+const getRandomDecimal = (from, to, precision) => {
   if (from == to) {
     return from;
   }
   if (from < 0 || from > to) {
-    return undefined
-  };
+    return undefined;
+  }
 
-  let randomInt = Math.random();
-  let randomNegNumber = randomInt * (from - to);
-  let result = randomNegNumber + to;
+  const randomInt = Math.random();
+  const randomNegNumber = randomInt * (from - to);
+  const result = randomNegNumber + to;
   return result.toFixed(precision);
-}
+};
+
+getRandomDecimal();
+getRandomInt();
