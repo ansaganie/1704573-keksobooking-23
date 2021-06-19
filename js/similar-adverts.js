0f7e306102a255e7c7c0ddb95d01738423d819be
ansagan <islamgaliev@mail.ru>
2021-06-19 22:36:23 +0500

    function 'generateCard' was created; elements of absent parameters are hidden visually

--- js/similar-adverts.js
+++ js/similar-adverts.js
@@ -2,7 +2,9 @@ import { getRandomAdvert } from './data.js';
 import {
   createFeaturesList,
   createPhotosList,
-  createRoomAndGuestText
+  createRoomAndGuestText,
+  updatePropertyOrHide,
+  appendChildOrHide
 } from './similar-adverts-utils.js';
 
 const RANDOM_ADVERT_COUNT = 10;
@@ -16,37 +18,60 @@ const randomAdverts = new Array(RANDOM_ADVERT_COUNT)
   .fill(null)
   .map(getRandomAdvert);
 
-const similarListFragment = new Array();
-
-randomAdverts.forEach((advert) => {
+const generateCard = ({ author, offer }) => {
   const newCard = cardTemplate.cloneNode(true);
-  const offer = advert.offer;
 
   newCard.querySelector('.popup__title').textContent = offer.title;
-  newCard.querySelector('.popup__text--address').textContent = offer.address;
   newCard.querySelector(
     '.popup__text--price',
   ).innerHTML = `${offer.price} <span>₽/ночь</span>`;
-  newCard.querySelector('.popup__type').textContent = offer.type;
-  newCard.querySelector('.popup__text--capacity').textContent =
-    createRoomAndGuestText(offer.rooms, offer.guests);
-  newCard.querySelector(
-    '.popup__text--time',
-  ).textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  const features = newCard.querySelector('.popup__features');
-  features.textContent = '';
-  features.appendChild(createFeaturesList(offer.features));
 
-  newCard.querySelector('.popup__description').textContent = offer.description;
+  updatePropertyOrHide(
+    newCard.querySelector('.popup__text--address'),
+    'textContent',
+    offer.address,
+  );
+
+  updatePropertyOrHide(
+    newCard.querySelector('.popup__type'),
+    'textContent',
+    offer.type,
+  );
+
+  updatePropertyOrHide(
+    newCard.querySelector('.popup__text--capacity'),
+    'textContent',
+    createRoomAndGuestText(offer.rooms, offer.guests),
+  );
+
+  updatePropertyOrHide(
+    newCard.querySelector('.popup__text--time'),
+    'textContent',
+    `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`,
+  );
+
+  appendChildOrHide(
+    newCard.querySelector('.popup__features'),
+    createFeaturesList(offer.features),
+  );
+
+  updatePropertyOrHide(
+    newCard.querySelector('.popup__description'),
+    'textContent',
+    offer.description,
+  );
 
   const photos = newCard.querySelector('.popup__photos');
   const imgTemplate = photos.querySelector('.popup__photo');
-  photos.textContent = '';
-  photos.appendChild(createPhotosList(offer.photos, imgTemplate));
+  appendChildOrHide(photos, createPhotosList(offer.photos, imgTemplate));
 
-  newCard.querySelector('.popup__avatar').src = advert.author.avatar;
-  similarListFragment.push(newCard);
-});
+  updatePropertyOrHide(
+    newCard.querySelector('.popup__avatar'),
+    'src',
+    author.avatar,
+  );
 
-mapCanvas.appendChild(similarListFragment[0]);
+  return newCard;
+};
 
+mapCanvas.appendChild(generateCard(randomAdverts[0]));
