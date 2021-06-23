61c7ec2f1c0fe4be783e9c44926a9493d64444cd
ansagan <islamgaliev@mail.ru>
2021-06-24 00:01:13 +0500

    title validation logic to separate module

---
+++ js/form-validate-title.js
@@ -0,0 +1,30 @@
+const TITLE_MIN_LENGTH = 30;
+const TITLE_MAX_LENGTH = 100;
+const title = document.querySelector('#title');
+
+const validateTitle = () => {
+  const validity = title.validity;
+  const messageElement = title.nextElementSibling;
+  const length = title.value.length;
+
+  if (validity.valueMissing) {
+    messageElement.textContent = 'Это обязательное поле';
+    return false;
+  }
+
+  if (validity.tooShort) {
+    messageElement.textContent = `Минимум 30 символов, не хватает еще ${
+      TITLE_MIN_LENGTH - length
+    }`;
+    return false;
+  }
+
+  if (validity.tooLong) {
+    messageElement.textContent = `Максимум ${TITLE_MAX_LENGTH} символов`;
+    return false;
+  }
+  messageElement.textContent = '';
+  return true;
+};
+
+export { validateTitle, title };
