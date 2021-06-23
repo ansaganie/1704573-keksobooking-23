e96d9af25673241191815385ed1c00502b9e6b38
ansagan <islamgaliev@mail.ru>
2021-06-24 00:01:26 +0500

    type validation logic to separate module

---
+++ js/form-validate-type.js
@@ -0,0 +1,12 @@
+const TYPE_MIN_PRICE = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+
+const type = document.querySelector('#type');
+
+export { type, TYPE_MIN_PRICE};
+
