dd7e8fe8526b47d991849687d7ca29c72b2b7297
ansagan <islamgaliev@mail.ru>
2021-06-24 00:00:07 +0500

    as the volume of code in form.js module was increasing it was devided into several modules; connection between appartment type and price was established

--- js/form.js
+++ js/form.js
@@ -1,121 +1,23 @@
-import { disableOptions } from './form-utils.js';
-
-const TITLE_MIN_LENGTH = 30;
-const TITLE_MAX_LENGTH = 100;
-const PRICE_MAX_VALUE = '1 000 000';
-const NUMBER_PATTERN = /^\D+$/;
+import { price, validatePrice } from './form-validate-price.js';
+import { type, TYPE_MIN_PRICE } from './form-validate-type.js';
+import { title, validateTitle } from './form-validate-title.js';
+import {
+  validateRoomAndGuestNumber,
+  synchronizeRoomAndGuestNumber,
+  roomNumber,
+  guestNumber
+} from './form-validate-room-guest.js';
 
 const advertForm = document.querySelector('.ad-form');
 const formSubmitButton = advertForm.querySelector('.ad-form__submit');
 
 const address = advertForm.querySelector('#address');
-const title = advertForm.querySelector('#title');
-const price = advertForm.querySelector('#price');
-const roomNumber = advertForm.querySelector('#room_number');
-const guestNumber = advertForm.querySelector('#capacity');
+
+//const timein = advertForm.querySelector('#timein');
+//const timeout = advertForm.querySelector('#timeout');
 
 //this is a temporary value shows the coordinates of the center of Tokyo
 address.value = '35.65858 139.74543';
-
-const validateRoomAndGuestNumber = () => {
-  const roomValue = roomNumber.value;
-  const guestValue = guestNumber.value;
-  const messageElement = guestNumber.nextElementSibling;
-
-  switch (roomValue) {
-    case '1':
-      if (guestValue !== '1') {
-        messageElement.textContent = '1 комната = 1 гость';
-        return false;
-      }
-      break;
-    case '2':
-      if (guestValue !== '1' && guestValue !== '2') {
-        messageElement.textContent = '2 комнаты = 1 или 2 гостей';
-        return false;
-      }
-      break;
-    case '3':
-      if (guestValue === '0') {
-        messageElement.textContent = '3 комнаты = 1, 2 или 3 гостей';
-        return false;
-      }
-      break;
-    case '100':
-      if (guestValue !== '0') {
-        messageElement.textContent = '100 комнат не для гостей';
-        return false;
-      }
-      break;
-  }
-  messageElement.textContent = '';
-  return true;
-};
-
-const synchronizeRoomAndGuestNumber = () => {
-  const options = guestNumber.children;
-  switch (roomNumber.value) {
-    case '1':
-      disableOptions(options, '1');
-      break;
-    case '2':
-      disableOptions(options, '1', '2');
-      break;
-    case '3':
-      disableOptions(options, '1', '2', '3');
-      break;
-    case '100':
-      disableOptions(options, '0');
-      break;
-  }
-  validateRoomAndGuestNumber();
-};
-
-const validateTitle = () => {
-  const validity = title.validity;
-  const messageElement = title.nextElementSibling;
-  const length = title.value.length;
-
-  if (validity.valueMissing) {
-    messageElement.textContent = 'Это обязательное поле';
-    return false;
-  }
-
-  if (validity.tooShort) {
-    messageElement.textContent = `Минимум 30 символов, не хватает еще ${
-      TITLE_MIN_LENGTH - length
-    }`;
-    return false;
-  }
-
-  if (validity.tooLong) {
-    messageElement.textContent = `Максимум ${TITLE_MAX_LENGTH} символов`;
-    return false;
-  }
-  messageElement.textContent = '';
-  return true;
-};
-
-const validatePrice = () => {
-  const validity = price.validity;
-  const messageElement = price.nextElementSibling;
-  if (validity.valueMissing) {
-    messageElement.textContent = 'Это обязательное поле';
-    return false;
-  }
-
-  if (validity.rangeOverflow) {
-    messageElement.textContent = `Максимальная цена ${PRICE_MAX_VALUE}`;
-    return false;
-  }
-
-  if (NUMBER_PATTERN.test(price.value)) {
-    messageElement.textContent = 'Цена указывается в цифрах';
-    return false;
-  }
-  messageElement.textContent = '';
-  return true;
-};
 
 synchronizeRoomAndGuestNumber();
 
@@ -131,6 +33,11 @@ title.addEventListener('blur', () => {
 price.addEventListener('blur', () => {
   validatePrice();
   price.addEventListener('input', validatePrice);
+});
+
+type.addEventListener('change', () => {
+  price.placeholder = TYPE_MIN_PRICE[type.value];
+  price.min = TYPE_MIN_PRICE[type.value];
 });
 
 formSubmitButton.addEventListener('click', (evt) => {
