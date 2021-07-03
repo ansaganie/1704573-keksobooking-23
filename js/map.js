bc45fe899949ea454c9c920221510d8c86ad5562
ansagan <islamgaliev@mail.ru>
2021-07-03 19:24:17 +0500

    resetMap function was added

--- js/map.js
+++ js/map.js
@@ -6,8 +6,10 @@ const OPEN_STREET_MAP_ATTR = '&copy; <a href="https://www.openstreetmap.org/copy
 
 const MAIN_ICON_HEIGHT = 52;
 const MAIN_ICON_WIDTH = 52;
+const MAIN_ICON_LINK = '../img/main-pin.svg';
 const POPUP_ICON_HEIGHT = 40;
 const POPUP_ICON_WIDTH = 40;
+const POPUP_ICON_LINK = '../img/pin.svg';
 const ANCHOR_X = 26;
 const ANCHOR_Y = 52;
 const POPUP_ANCHOR_X = 20;
@@ -22,8 +24,10 @@ const mapContainer = document.querySelector('#map-canvas');
 const address = document.querySelector('#address');
 
 const map = L.map(mapContainer);
+const layer = L.layerGroup().addTo(map);
+
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: MAIN_ICON_LINK,
   iconSize: [MAIN_ICON_HEIGHT, MAIN_ICON_WIDTH],
   iconAnchor: [ANCHOR_X, ANCHOR_Y],
 });
@@ -40,26 +44,20 @@ const mainPinMarker = L.marker(
 );
 
 const popupIcon = L.icon({
-  iconUrl: '../img/pin.svg',
+  iconUrl: POPUP_ICON_LINK,
   iconSize: [POPUP_ICON_HEIGHT, POPUP_ICON_WIDTH],
   iconAnchor: [POPUP_ANCHOR_X, POPUP_ANCHOR_Y],
 });
 
-const layer = L.layerGroup().addTo(map);
 
 const createPopups = (adverts) => {
   adverts.forEach((advert) => {
-    const { lat, lng } = advert.location;
     const marker = L.marker(
-      {
-        lat,
-        lng,
-      },
+      advert.location,
       {
         popupIcon,
       },
     );
-
     marker.addTo(layer).bindPopup(generateCard(advert));
   });
 };
@@ -69,20 +67,20 @@ const changeAddressValue = ({ target }) => {
   address.value = `${latlng.lat.toFixed(5)} ${latlng.lng.toFixed(5)}`;
 };
 
-map.on('load', activatePage).setView(
-  {
-    lat: TOKYO_CENTER.lat,
-    lng: TOKYO_CENTER.lng,
-  },
-  SCALE,
-);
+const resetMap = ()=> {
+  mainPinMarker.setLatLng(TOKYO_CENTER);
+  map.setView(TOKYO_CENTER, SCALE);
+};
+
+map.on('load', activatePage).setView(TOKYO_CENTER, SCALE);
 
 L.tileLayer( MAP_PROVIDER_LINK, {
   attribution: OPEN_STREET_MAP_ATTR,
 }).addTo(map);
 
-mainPinMarker.on('add', changeAddressValue);
 mainPinMarker.on('drag', changeAddressValue);
 mainPinMarker.addTo(map);
 
-export { createPopups };
+export { createPopups,
+  resetMap
+};
