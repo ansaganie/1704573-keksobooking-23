b93781cbc73846670c823e9e40c7475a0d4f290c
ansagan <islamgaliev@mail.ru>
2021-07-03 13:40:02 +0500

    If any error from server it shows popup notification

--- js/api.js
+++ js/api.js
@@ -1,9 +1,20 @@
 import { createPopups } from './map.js';
+import { showServerErrorMessage } from './utils.js';
 
 const DATA_FETCH_LINK = 'https://23.javascript.pages.academy/keksobooking/data';
 
 fetch(DATA_FETCH_LINK)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Data was not retreived');
+    } else {
+      return response;
+    }
+  })
   .then((response) => response.json())
   .then((adverts) => {
     createPopups(adverts);
+  })
+  .catch(() => {
+    showServerErrorMessage();
   });
