3e726fa698cdb3aaa6ad8798111b3a8674373fae
ansagan <islamgaliev@mail.ru>
2021-07-04 00:10:38 +0500

    Module for address input validation

---
+++ js/form/form-validate-address.js
@@ -0,0 +1,16 @@
+
+const ADDERSS_PATTERN = /^([0-9]+\.?[0-9]*|\.[0-9]+) ([0-9]+\.?[0-9]*|\.[0-9]+)$/;
+const address = document.querySelector('#address');
+
+const validateAddress = () => {
+  const messageElement = address.nextElementSibling;
+
+  if (!ADDERSS_PATTERN.test(address.value)) {
+    messageElement.textContent = 'Неправильный формат адреса';
+    return false;
+  }
+
+  return true;
+};
+
+export { address, validateAddress };
