8277966ffc5e0f58544f6fab7b637a7b9626174e
ansagan <islamgaliev@mail.ru>
2021-07-04 00:14:33 +0500

    Submit button's click event listener was updated: it sends data to server without updating the page

--- js/form/form.js
+++ js/form/form.js
@@ -12,6 +12,9 @@ import {
   roomNumber
 } from './form-validate-room-capacity.js';
 import { resetMap } from '../map/map.js';
+import { validateAddress } from './form-validate-address.js';
+import { showErrorMessage, showSuccessMessage } from './success-error.js';
+import { sendData } from '../api.js';
 
 const advertForm = document.querySelector('.ad-form');
 const formSubmitButton = advertForm.querySelector('.ad-form__submit');
@@ -42,6 +45,20 @@ const resetForm = () => {
   resetValidationMessages();
 };
 
+const submitFrom = (evt) => {
+  evt.preventDefault();
+
+  const isTitleValid = validateTitle();
+  const isPriceValid = validatePrice();
+  const isRoomAndGuestNumberValid = validateRoomNumberAndCapacity();
+  const isAddressValid = validateAddress();
+
+  if (isTitleValid && isPriceValid
+     && isRoomAndGuestNumberValid && isAddressValid) {
+    sendData(new FormData(advertForm), showSuccessMessage, showErrorMessage);
+  }
+};
+
 synchronizeRoomNumberAndCapacity();
 
 formResetButton.addEventListener('click', resetForm);
@@ -64,12 +81,8 @@ timeout.addEventListener('change', () => {
   timein.value = timeout.value;
 });
 
-formSubmitButton.addEventListener('click', (evt) => {
-  const isTitleValid = validateTitle();
-  const isPriceValid = validatePrice();
-  const isRoomAndGuestNumberValid = validateRoomNumberAndCapacity();
+formSubmitButton.addEventListener('click', submitFrom);
 
-  if (!isTitleValid || !isPriceValid || !isRoomAndGuestNumberValid) {
-    evt.preventDefault();
-  }
-});
+export {
+  resetForm
+};
