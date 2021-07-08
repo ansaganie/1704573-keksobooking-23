ff5caffd9b7ee4ebfda5557e48be454a8db257bf
ansagan <islamgaliev@mail.ru>
2021-07-08 17:03:00 +0500

    page state is imported earlier than other modules; added new function to activate map filter form separately

--- js/page-state.js
+++ js/page-state.js
@@ -1,25 +1,22 @@
-const disableOrEnableElements = (disabled, ...elements) => {
+const advertForm = document.querySelector('.ad-form');
+const mapFilters = document.querySelector('.map__filters');
+
+const setDisabled = (disabled, ...elements) => {
   elements.forEach((elem) => (elem.disabled = disabled));
 };
 
-const deactivatePage = () => {
-  const advertForm = document.querySelector('.ad-form');
-  const mapFilters = document.querySelector('.map__filters');
-  advertForm.classList.add('ad-form--disabled');
-  mapFilters.classList.add('ad-form--disabled');
-  disableOrEnableElements(true, ...advertForm.children, ...mapFilters.children);
-};
+advertForm.classList.add('ad-form--disabled');
+mapFilters.classList.add('ad-form--disabled');
+setDisabled(true, ...advertForm.children, ...mapFilters.children);
 
-const activatePage = () => {
-  const advertForm = document.querySelector('.ad-form');
-  const mapFilters = document.querySelector('.map__filters');
+const activateMainForm = () => {
   advertForm.classList.remove('ad-form--disabled');
-  mapFilters.classList.remove('ad-form--disabled');
-  disableOrEnableElements(
-    false,
-    ...advertForm.children,
-    ...mapFilters.children,
-  );
+  setDisabled(false, ...advertForm.children);
 };
 
-export { deactivatePage, activatePage };
+const activateMapFilters = () => {
+  mapFilters.classList.remove('ad-form--disabled');
+  setDisabled(false, ...mapFilters.children);
+};
+
+export { activateMainForm, activateMapFilters };
