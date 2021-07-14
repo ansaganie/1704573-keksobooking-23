43e86ee2d81462f8ccbed2511ae6f7a4133d7395
ansagan <islamgaliev@mail.ru>
2021-07-14 10:58:57 +0500

    Magic values were moved to constants; Function parameter namings were changed

--- js/map/filter.js
+++ js/map/filter.js
@@ -1,11 +1,15 @@
 import { drawPopups } from './map.js';
 import { adverts } from '../api.js';
 
+const PRICE_LOW = 10000;
+const PRICE_MIDDLE = 10000;
+const PRICE_HIGH = 50000;
+
 const priceFilters = {
   'any' : () => true,
-  'low' : (priceGiven) => priceGiven < 10000 && priceGiven >= 0,
-  'middle' : (priceGiven) => priceGiven >= 10000 && priceGiven < 50000,
-  'high' : (priceGiven) => priceGiven >= 50000,
+  'low' : (priceGiven) => priceGiven < PRICE_LOW && priceGiven >= 0,
+  'middle' : (priceGiven) => priceGiven >= PRICE_MIDDLE && priceGiven < PRICE_HIGH,
+  'high' : (priceGiven) => priceGiven >= PRICE_HIGH,
 };
 
 const mapFilters = document.querySelector('.map__filters');
@@ -15,9 +19,9 @@ const rooms = mapFilters.querySelector('#housing-rooms');
 const guests = mapFilters.querySelector('#housing-guests');
 const features = mapFilters.querySelector('#housing-features');
 
-const retrieveFeaturesNames = (array) => {
+const retrieveFeaturesNames = (inputs) => {
   const result = [];
-  for (const elem of array) {
+  for (const elem of inputs) {
     if (elem.checked) {
       result.push(elem.value);
     }
@@ -26,51 +30,48 @@ const retrieveFeaturesNames = (array) => {
   return result;
 };
 
-const doFilter = (data) => {
-  const filtered = data
-    .filter((advert) => {
-      const offer = advert.offer;
+const doFilter = (data) => data
+  .filter((advert) => {
+    const offer = advert.offer;
 
-      if (offer.type !== type.value && type.value !== 'any') {
+    if (offer.type !== type.value && type.value !== 'any') {
+      return false;
+    }
+
+    if (!priceFilters[price.value](offer.price)) {
+      return false;
+    }
+
+    if (
+      offer.rooms !== undefined
+      && offer.rooms !== +rooms.value
+      && rooms.value !== 'any'
+    ) {
+      return false;
+    }
+
+    if (
+      offer.guests !== undefined
+      && offer.guests !== +guests.value
+      && guests.value !== 'any'
+    ) {
+      return false;
+    }
+
+    const featuresNames = retrieveFeaturesNames(features.children);
+
+    if (featuresNames.length > 0 && offer.features === undefined) {
+      return false;
+    }
+
+    for (const elem of featuresNames) {
+      if (!offer.features.includes(elem)) {
         return false;
       }
+    }
 
-      if (!priceFilters[price.value](offer.price)) {
-        return false;
-      }
-
-      if (
-        offer.rooms !== undefined
-        && offer.rooms !== +rooms.value
-        && rooms.value !== 'any'
-      ) {
-        return false;
-      }
-
-      if (
-        offer.guests !== undefined
-        && offer.guests !== +guests.value
-        && guests.value !== 'any'
-      ) {
-        return false;
-      }
-
-      const featuresArray = retrieveFeaturesNames(features.children);
-
-      if (featuresArray.length > 0 && offer.features === undefined) {
-        return false;
-      }
-
-      for (const elem of featuresArray) {
-        if (!offer.features.includes(elem)) {
-          return false;
-        }
-      }
-
-      return true;
-    });
-  return filtered;
-};
+    return true;
+  });
 
 const clearFilter = () => {
   mapFilters.reset();
